4334835e7dd220e3f2da030028943ce204161c66
Kusasalethu Sithole <skusasalethu@gmail.com>
2019-09-17 19:50:36 +0200

    Delete test.py

--- raw_vector_data_source_prepping/test.py
+++
@@ -1,7 +0,0 @@
-filename = '/tmp/qgis.log'
-
-def write_log_message(message, tag, level):
-    with open(filename, 'a') as logfile:
-        logfile.write('{tag}({level}): {message}'.format(tag=tag, level=level, message=message))
-
-QgsApplication.messageLog().messageReceived.connect(write_log_message)
